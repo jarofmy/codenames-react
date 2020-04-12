d3cef36986a8246c1b89c7af1423a3962a3c8c96
Jeremy Van <jeremyvan614@gmail.com>
2020-04-12 12:37:20 -0500

    init codenames react project

---
+++ src/components/Cell.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import './Cell.css';
+
+class Cell extends React.Component {
+
+    handleClick(word, color) {
+        // Update element class color
+        var card = document.getElementById(word);
+        card.className = color;
+        // Manage color count
+        if(!card.value) {
+            this.props.colorCount(color);
+            card.value = true;
+        }
+    }
+
+    render() {
+        return (
+            <div className={this.props.className}>
+                <Card 
+                variant="outlined" 
+                onClick={() => this.handleClick(this.props.word, this.props.color)} 
+                id={this.props.word} 
+                className='Card'
+            >
+                <CardContent className='Card-content'>
+                    <Typography variant="h5">
+                        {this.props.word}
+                    </Typography>
+                </CardContent>
+            </Card>
+            </div>
+        )
+    }
+};
+
+export default Cell;
